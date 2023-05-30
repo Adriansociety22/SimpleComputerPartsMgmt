fa720e3bea6eda4f9ce107aa28e6d6daa2808e0b
Adriansociety22 <124018721+Adriansociety22@users.noreply.github.com>
2023-05-30 18:26:57 +0800

    mysql scripts for the application

---
+++ mysql scripts/01 - CREATE TABLE computerparts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE computerparts (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    PartName VARCHAR(500),
+    PartType VARCHAR(500)
+);
---
+++ mysql scripts/02 - INSERT INTO computerparts.sql
@@ -0,0 +1,4 @@
+INSERT INTO computerparts (PartName, PartType)
+VALUES ('Keyboard', 'Input Device'),
+       ('Monitor', 'Output Device'),
+       ('Hard Drive', 'Storage Device');
